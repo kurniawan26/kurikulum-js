17b76e9a12a328a58513d629ee33635f38f53a58
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2021-08-13 19:24:08 +0700

    get rid of index.js

--- Pertemuan 1/index.js
+++ Pertemuan 1/index.js
@@ -1,17 +1,1 @@
-const heading = document.querySelector(".heading-primary");
-console.log(heading);
-heading.textContent = "Helo";
 
-const heading2 = document.querySelector("#paragraph");
-console.log(heading2);
-heading2.textContent = "World";
-
-const createElement = document.createElement("h3");
-createElement.textContent = "Hello World";
-
-const main = document.querySelector(".main-section")
-main.appendChild(createElement)
-
-let age = 25;
-
-console.log(age === +"25");
